d708fac5be88f7c04512177a35900ee303790e6e
myaasiinh <myaasiinh@gmail.com>
2025-06-05 08:24:11 +0700

    refactor: improve comments and formatting in slider_config.dart for clarity

--- lib/third_party/slider_config.dart
+++ lib/third_party/slider_config.dart
@@ -1,7 +1,12 @@
-// lib/utils/slider_config.dart
-const List<String> lottieAnimationPaths 
-= [
+// File: lib/utils/slider_config.dart
+
+// Mendefinisikan daftar konstan berisi path ke file animasi Lottie,
+// yang akan digunakan untuk menampilkan animasi dalam aplikasi.
+const List<String> lottieAnimationPaths = [
+  // Path ke file animasi Lottie untuk karakter Tigger (berkas JSON).
   'assets/lottie/tigger.json',
+  // Path ke file animasi Lottie untuk animasi burung (berkas JSON).
   'assets/lottie/bird.json',
+  // Path ke file animasi Lottie untuk animasi mobil (berkas JSON).
   'assets/lottie/car.json',
 ];
